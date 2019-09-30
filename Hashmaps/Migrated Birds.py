b8e6063a48d34a0b4c5f6bfd2b9ddc646f093df6
Ziyang Ma <maziyang1008@gmail.com>
2019-09-30 16:38:45 -0400

    migrated birds

---
+++ Hashmaps/Migrated Birds.py
@@ -0,0 +1,17 @@
+
+def migratoryBirds(arr):
+    types=list(set(arr))
+    count=dict((x,0) for x in types)
+    for i in range(len(arr)):
+        if arr[i] in count:
+            count[arr[i]] +=1
+    
+    return min([x for x in count.keys() if count[x]==max(count.values())])
+
+
+
+def migratoryBirds2(arr):
+    count = [0]*len(arr)
+    for t in arr:
+         count[t] += 1
+    return count.index(max(count))
