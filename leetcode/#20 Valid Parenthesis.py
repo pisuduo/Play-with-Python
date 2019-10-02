ed853fcd641c36e2b42af972430e0a58ab7a123c
Ziyang Ma <maziyang1008@gmail.com>
2019-10-01 22:38:47 -0400

    first commit

---
+++ leetcode/#20 Valid Parenthesis.py
@@ -0,0 +1,27 @@
+### using stack data structure ###
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    if len(s)%2 !=0:
+        return False
+
+    stack=[]
+    map={')':'(','}':'{',']':'['}
+    for x in s:
+        if x in map:  ## x is a closing bracket
+            top=stack.pop() if stack else '$'
+            if top!=map[x]: return False
+        else:
+            stack.append(x)
+    
+    return stack==[]
+
+
+s='(((([[}]]))))'
+s1='{[(())]}'
+isValid(s)
+isValid(s1)
+
+
